70da23fbbbedb8ba53a0c06992f0a07a25c2cd06
Joe Saddigh <joesaddigh@gmail.com>
2023-02-17 10:56:18 +0000

    CropGenRunningDocker: Change SocketClient to support reading/writing socket data by expecting the message size to be received/sent before the data.

--- lib/socket/socket_client.py
+++ lib/socket/socket_client.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from lib.socket.socket_client_base import SocketClientBase
 from lib.models.error_message import ErrorMessage
 
@@ -6,6 +8,7 @@ from lib.models.error_message import ErrorMessage
 #
 class SocketClient(SocketClientBase):
     ENCODING = 'utf-8'
+    LITTLE_ENDIAN = "little"
 
     #
     # Constructor
@@ -20,6 +23,7 @@ class SocketClient(SocketClientBase):
     # Connect method
     #
     def connect(self):
+        #client_reader, client_writer = yield from asyncio.open_connection("host", 9999)
         raise NotImplementedError("connect not implemented")
     
     #
@@ -38,14 +42,11 @@ class SocketClient(SocketClientBase):
     # Send data method
     #
     async def send_text_async(self, data):
-        # Encode the data to ascii.
-        encoded_data = data.encode(SocketClient.ENCODING)
-        # Store the length.        
-        encoded_data_length = len(encoded_data)
-        # Send the length of the encoded data.
-        self.writer.write(str(encoded_data_length).encode(SocketClient.ENCODING))
-        # Now the data.
-        self.writer.write(encoded_data)
+        # Send the length of the encoded data as a byte array.
+        message_size_byte_array = len(data).to_bytes(4, SocketClient.LITTLE_ENDIAN)
+        self.writer.write(message_size_byte_array)
+        # Now send the data.
+        self.writer.write(data.encode(SocketClient.ENCODING))
         await self.writer.drain()
 
     #
@@ -69,7 +70,8 @@ class SocketClient(SocketClientBase):
     #
     # Receive data method
     #
-    async def receive_text_async(self):
-        return await self.reader.read(
-            self.config.socket_receive_buffer_size
-        )
+    async def receive_text_async(self, encoding=ENCODING):
+        message_size_byte_array = await self.reader.read(4)
+        message_size_bytes = int.from_bytes(message_size_byte_array, SocketClient.LITTLE_ENDIAN)
+        message_data = await self.reader.readexactly(message_size_bytes)
+        return message_data
