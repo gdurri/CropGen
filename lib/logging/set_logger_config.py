bebfc8bb6026cae9f239c4a521ef3aee3f76a62b
Joe Saddigh <joesaddigh@gmail.com>
2023-03-11 09:23:04 +0000

    add-run-cropgen-api: Change logging to use RotatingFileHandler, max 2MB, max 2 files.

--- lib/logging/set_logger_config.py
+++ lib/logging/set_logger_config.py
@@ -1,6 +1,11 @@
 import logging.config
 import os.path
 
+# Create the logs directory for our logger to log to.
+root_directory = os.path.join(os.path.dirname(__file__), "..\\..\\logs")
+if not os.path.exists(root_directory):
+    os.makedirs(root_directory)
+
 # Simple script setup the logger using the config ini file.
 logger_config = "logger_config.ini"
 logging_config_path = os.path.join(os.path.dirname(__file__), logger_config)
