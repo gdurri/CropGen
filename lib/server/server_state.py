e7c51feb7115608d14a3331114d98a8a6fc1fa39
Joe Saddigh <joesaddigh@gmail.com>
2023-05-18 14:25:05 +0100

    Minor tweak to make ServerState locking code clearer.

--- lib/server/server_state.py
+++ lib/server/server_state.py
@@ -17,17 +17,15 @@ class ServerState():
     # Get whether a job is currently running.
     #
     def get_is_job_currently_running(self):
-        running_job_id = self.get_running_job_id()
-        return running_job_id != ''
+        with self.lock:
+            return self.running_job != ''
 
     #
     # Get the JobID of the running job.
     #
     def get_running_job_id(self):
-        running_job_id = ''
         with self.lock:
-            running_job_id = self.running_job
-        return running_job_id
+            return self.running_job
 
     #
     # Set the running job.
@@ -42,4 +40,5 @@ class ServerState():
     #
     def clear_running_job_id(self):
         logging.info("Clearing the current running job: '%s'", self.running_job)
-        self.set_running_job_id('')
+        with self.lock:
+            self.running_job = ''
