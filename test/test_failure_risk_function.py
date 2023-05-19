65314e1a90aad65e9d993092ca757bc86eb3273b
Joe Saddigh <joesaddigh@gmail.com>
2023-05-19 08:35:37 +0100

    Add extra tests.

--- test/test_failure_risk_function.py
+++ test/test_failure_risk_function.py
@@ -16,14 +16,22 @@ class TestAggregateFunction():
 class FailureRiskFunctionTests(TestBase):
 
     @parameterized.expand([
-        ("<", "10", 0.75),
-        ("<=", "10", 1),
-        (">",  "5", 0.5),
-        (">=", "8", 0.5),
-        ("==", "2", 0.25),
-        ("!=", "8", 0.75)
+        ("<",  "10",    0, 0.75),
+        ("<",  "1",     0, 0),
+        ("<",  "8",     0, 0.5),
+        ("<=", "10",    0, 1),
+        ("<=", "8",     0, 0.75),
+        (">",  "5",     0, 0.5),
+        (">",  "1",     0, 0.75),
+        (">=", "8",     0, 0.5),
+        (">=", "3",     0, 0.5),
+        ("==", "2",     0, 0.25),
+        ("==", "100",   0, 0),
+        ("==", "-90",   0, 0),
+        ("!=", "8",     0, 0.75),
+        ("!=", "100",   0, 1)
     ])
-    def test_calculate(self, operator, param, expected):
+    def test_calculate(self, operator, param, apsim_output_index, expected):
         # Arrange
         aggregate_function = TestAggregateFunction([operator, param])
         results_for_individual = [
@@ -32,7 +40,6 @@ class FailureRiskFunctionTests(TestBase):
             self.create_apsim_result([8, 0, 0, 0, 0]),
             self.create_apsim_result([10, 0, 0, 0, 0])
         ]
-        apsim_output_index = 0
 
         # Act
         actual = FailureRiskFunction.calculate(aggregate_function, results_for_individual, apsim_output_index)
