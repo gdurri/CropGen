c477737cb71703cedd94625152fba689671244ea
Joe Saddigh <joesaddigh@gmail.com>
2023-07-02 08:51:14 +0100

    Modify script to cleanse the csv input just in case any strange characters have crept in.

--- lib/utility_scripts/environment_type_csv_convert.py
+++ lib/utility_scripts/environment_type_csv_convert.py
@@ -3,11 +3,20 @@ import csv
 import json
 import sys
 
+def remove_strange_characters(s):
+    # Define the set of characters you want to allow in the CSV file
+    allowed_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,")
+    
+    # Filter out any characters not present in the allowed set
+    filtered_string = ''.join(filter(allowed_characters.__contains__, s))
+    
+    return filtered_string
+
 def csv_to_json(csv_file, json_file):
     data = {"EnvironmentTypes": []}
 
     with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file)
+        reader = csv.DictReader((remove_strange_characters(line) for line in file))
         for row in reader:
             environment_type = {
                 "SimulationName": row['SimulationName'],
@@ -29,10 +38,15 @@ if __name__ == '__main__':
         sys.exit(1)
 
     csv_file = sys.argv[1]
+    # Get the directory path of the currently running script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     csv_filename = os.path.basename(csv_file)
-    json_file = os.path.splitext(csv_filename)[0] + '.json'
 
-    if not json_file.startswith('output/'):
-        json_file = 'output/' + json_file
+    # Create the output directory if it doesn't exist
+    output_dir = os.path.join(script_dir, 'output')
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Create the JSON file path by prepending the output directory to the base name of the CSV file
+    json_file = os.path.join(output_dir, os.path.splitext(csv_filename)[0] + '.json')
 
     csv_to_json(csv_file, json_file)
