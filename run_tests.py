2be05cf6f569e96acda8652ff50965efbc40df84
joesaddigh <joesaddigh@gmail.com>
2023-11-06 16:10:07 +1000

    CI Test 3.

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,18 @@
 import unittest
 import test.all_tests
+import coverage
 
 testSuite = test.all_tests.create_test_suite()
+
+# Create a coverage object
+cov = coverage.Coverage()
+cov.start()
+
 result = unittest.TextTestRunner().run(testSuite)
 
+# Stop and save the coverage data
+cov.stop()
+cov.save()
+
 if not result.wasSuccessful():
-    exit(1)
+    sys.exit(1)
