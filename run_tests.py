a107f0b84fffd25d4bac51d181361add2c0082fc
joesaddigh <joesaddigh@gmail.com>
2023-11-06 16:38:59 +1000

    Remove attempt at code coverage.

--- run_tests.py
+++ run_tests.py
@@ -1,18 +1,8 @@
 import unittest
 import test.all_tests
-import coverage
-import sys
 
 testSuite = test.all_tests.create_test_suite()
-
-# Create a coverage object
-cov = coverage.Coverage()
-cov.start()
-
 result = unittest.TextTestRunner().run(testSuite)
-
-cov.stop()
-cov.save()
 
 if not result.wasSuccessful():
     sys.exit(1)
