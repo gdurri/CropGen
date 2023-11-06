81c28d5ecd373a4351e4edfe25544a6fa8946f0d
joesaddigh <joesaddigh@gmail.com>
2023-11-06 16:33:41 +1000

    CI Test 5.

--- run_tests.py
+++ run_tests.py
@@ -11,11 +11,8 @@ cov.start()
 
 result = unittest.TextTestRunner().run(testSuite)
 
-# Stop and save the coverage data
 cov.stop()
-# Generate the HTML coverage report
 cov.save()
-cov.report(show_missing=False)
 
 if not result.wasSuccessful():
     sys.exit(1)
