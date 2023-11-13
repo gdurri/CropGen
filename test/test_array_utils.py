e532fa4c72614e4808f7809af23873dfb29ad345
Diva Nepal <lapen.avid@gmail.com>
2023-11-13 15:32:26 +1000

    Changed parametized expand

--- test/test_array_utils.py
+++ test/test_array_utils.py
@@ -4,29 +4,15 @@ from parameterized import parameterized
 from test.test_base import TestBase
 from test.helpers.apsim_result_helper import ApsimResultHelper
 from lib.utils.array_utils import ArrayUtils
-
+ 
 class ArrayUtilsTests(TestBase):
-    def test_split_arr_with_valid_max(self):
-        # Arrange
-        arr = [20, 23, 55, 77, 86, 99, 106]
-        max_val = 5
-        expected_result = [[20, 23, 55, 77, 86], [99, 106]]
- 
+    @parameterized.expand([
+        ([20, 23, 55, 77, 86, 99, 106], 5, [[20, 23, 55, 77, 86], [99, 106]]),
+        ([20, 23, 55, 77, 86, 99, 106], 10, [[20, 23, 55, 77, 86, 99, 106]]),
+    ])
+    def test_split_arr(self, arr, max_val, expected_result):
         # Act
         result = ArrayUtils._split_arr(arr, max_val)
- 
-        # Assert
-        self.assertEqual(result, expected_result)
- 
-    def test_split_arr_with_max_greater_than_array_length(self):
-        # Arrange
-        arr = [20, 23, 55, 77, 86, 99, 106]
-        max_val = 10
-        expected_result = [arr]  # The whole array should be in a single sub-array
- 
-        # Act
-        result = ArrayUtils._split_arr(arr, max_val)
- 
         # Assert
         self.assertEqual(result, expected_result)
  
